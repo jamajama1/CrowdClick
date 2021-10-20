b937438883d98a4899ebb807b8ba10c5e459e83d
jamajama1 <43017664+jamajama1@users.noreply.github.com>
2021-10-19 23:41:29 -0400

    Add project files.

---
+++ CrowdClick/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CrowdClick.Models;
+using Infrastucture.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdClick.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            ProductService productService = new ProductService();
+            var products = productService.GetProducts();
+            // strongly type model to pass in data from controller
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Infrastucture/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastucture.Services
+{
+    public class ProductService
+    {
+        public List<ProductResponseModel> GetProducts()
+        {
+            //call product repos and get products
+
+            var products = new List<ProductResponseModel> { 
+                new ProductResponseModel { Id=1, Title="TestProduct1", PosterUrl="https://sc04.alicdn.com/kf/H2a94aea1f4c44d918e6b8163a52a7026J.jpg" },
+                new ProductResponseModel { Id=2, Title="TestProduct2", PosterUrl="https://sc04.alicdn.com/kf/H3acb6b4f84af48118d4c2ccbf95477d6Z.jpg" },
+                new ProductResponseModel { Id=3, Title="TestProduct3", PosterUrl="https://sc04.alicdn.com/kf/Hbd8d3113f8724c14b8233de6e026eae9e.jpg" }
+            };
+
+            return products;
+        }
+    }
+}
